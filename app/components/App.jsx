6a2f2bba623184b1003724ddcba27f83b60bb135
davefw <davefw@gmail.com>
2015-12-04 17:09:19 +0000

    added editNote functionality

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -42,7 +42,28 @@ export default class App extends React.Component {
     });
   }
 
-  editNote(noteId, task) {
-    console.log('note edited', noteId, task);
+  editNote = (id, task) => {
+    const notes = this.state.notes;
+    const noteIndex = this.findNote(id);
+
+    if(noteIndex < 0) {
+      return;
+    }
+
+    notes[noteIndex].task = task;
+
+    // shorthand - {notes} is the same as {notes: notes}
+    this.setState({notes});
+  }
+
+  findNote = (id) => {
+    const notes = this.state.notes;
+    const noteIndex = notes.findIndex((note) => note.id === id);
+
+    if(noteIndex < 0) {
+      console.warn('Failed to find note', notes, id);
+    }
+
+    return noteIndex;
   }
 }
