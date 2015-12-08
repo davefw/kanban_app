a6180cb2d0c31071ff3d43348b074f3f214400bc
davefw <davefw@gmail.com>
2015-12-08 23:11:18 +0000

    local storage

--- app/index.jsx
+++ app/index.jsx
@@ -4,13 +4,17 @@ import './main.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
 
 main();
 
 function main() {
-  const app = document.createElement('div');
+	persist(alt, storage, 'app');
+	const app = document.createElement('div');
 
-  document.body.appendChild(app);
+	document.body.appendChild(app);
 
-  ReactDOM.render(<App />, app);
+	ReactDOM.render(<App />, app);
 }
